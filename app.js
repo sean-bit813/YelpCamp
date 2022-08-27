126a1cbd4c5f45d6454777c0c1b19c758de8b684
Jia, Baohui <bjia36@gatech.edu>
2022-08-26 17:37:20 -0700

    update styling

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 
 mongoose.connect(dbUrl).then(()=>console.log("DB Connected"))
-.catch((err)=>console.log(err));
+.catch((err)=>console.log(err))
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
