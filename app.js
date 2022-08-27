ecccc9061cbee0e1a3c1caaa570a38214129d388
Jia, Baohui <bjia36@gatech.edu>
2022-08-26 17:28:56 -0700

    update styling by bootstrap

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override');
 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+
+mongoose.connect(dbUrl).then(()=>console.log("DB Connected"))
+.catch((err)=>console.log(err));
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CampGroundSchema = new Schema ({
+const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
 
-module.exports = mongoose.model('Campground', CampGroundSchema);
+module.exports = mongoose.model('Campground', CampgroundSchema);
