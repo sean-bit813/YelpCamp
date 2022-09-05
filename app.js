c678426d9e4e56aff4a8dc351e0c0be0b5fcd4d1
sean-bit813 <74326705+sean-bit813@users.noreply.github.com>
2022-09-05 16:07:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ const MongoDBStore = require("connect-mongo")(session);
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect(dbUrl, {
-    useFindAndModify: false
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
